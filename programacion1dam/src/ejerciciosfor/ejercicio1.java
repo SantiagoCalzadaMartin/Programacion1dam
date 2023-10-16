0d5d4d76a583355e712529a947d96967ee26e80e
El Santi <santiago.calzada.martin@colegiolitterator.com>
2023-10-16 19:13:34 +0200

    creación del primer bucle for

--- programacion1dam/src/ejerciciosfor/ejercicio1.java
+++ programacion1dam/src/ejerciciosfor/ejercicio1.java
@@ -2,4 +2,14 @@ package ejerciciosfor;
 
 public class ejercicio1 {
 
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		for(int i = 1; i<101; i++) {
+			
+			System.out.println("i vale " + i + " ");
+		}
+
+	}
+
 }
--- programacion1dam/src/ejerciciosswitch/ejercicio1.java
+++ programacion1dam/src/ejerciciosswitch/ejercicio1.java
@@ -2,4 +2,26 @@ package ejerciciosswitch;
 
 public class ejercicio1 {
 
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int numero = 3;
+		
+		switch (numero) {
+		case 0: 
+			System.out.println("numero es 0");
+			break;
+		case 1:
+			System.out.println("numero es 1");
+			break;
+		case 2:
+			System.out.println("numero es 2");
+			break;
+		default:
+			System.out.println("numero es mayor que 2");
+		
+		}
+		
+	}
+
 }
