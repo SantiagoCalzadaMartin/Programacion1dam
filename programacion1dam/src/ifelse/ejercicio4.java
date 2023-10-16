7f779c48f5c27ad3c4064f28f4ae8f9ccb728d62
El Santi <santiago.calzada.martin@colegiolitterator.com>
2023-10-16 17:35:27 +0200

    cambio la variable a year.now

--- programacion1dam/src/ifelse/ejercicio4.java
+++ programacion1dam/src/ifelse/ejercicio4.java
@@ -1,5 +1,6 @@
 package ifelse;
 
+import java.time.Year;
 import java.util.Scanner;
 public class ejercicio4 {
 
@@ -7,7 +8,7 @@ public class ejercicio4 {
 		Scanner sc1 = new Scanner(System.in);
 		System.out.println("Introduzca su año de nacimiento");
 		int año1 = sc1.nextInt();
-		int año2 = 2023;
+		int año2 = Year.now (). getValue ();;
 		int resta = año2 - año1;
 		
 		if (año1>año2) {
