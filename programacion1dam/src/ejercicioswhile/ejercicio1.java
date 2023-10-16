a85a3819fdaef2a5e353bc273b646c544d04ab75
El Santi <santiago.calzada.martin@colegiolitterator.com>
2023-10-16 19:23:28 +0200

    ejercicios while

---
+++ programacion1dam/src/ejercicioswhile/ejercicio1.java
@@ -0,0 +1,20 @@
+package ejercicioswhile;
+
+public class ejercicio1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int i = 0;
+		
+		while (i<3) {
+			
+			System.out.println("i vale " + i);
+		
+		
+			i++;
+		}
+		
+	}
+
+}
