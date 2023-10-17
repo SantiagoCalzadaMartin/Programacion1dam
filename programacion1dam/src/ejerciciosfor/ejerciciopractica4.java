12073048560d39c7bde035af79496d695c460d6e
El Santi <santiago.calzada.martin@colegiolitterator.com>
2023-10-17 20:35:02 +0200

    ejercicios 4 de bucle for (inicio)

---
+++ programacion1dam/src/ejerciciosfor/ejerciciopractica4.java
@@ -0,0 +1,15 @@
+package ejerciciosfor;
+
+public class ejerciciopractica4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			
+	int n;
+	Scanner sc1 = new Scanner(System.in);
+	System.out.println("Introduce un número entero positivo para factorizarlo:");
+	n = sc1.NextInt();
+		
+	}
+
+}
