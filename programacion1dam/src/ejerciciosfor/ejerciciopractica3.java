8f290d30680ae0df7f66e81a8b5514785cd95214
El Santi <santiago.calzada.martin@colegiolitterator.com>
2023-10-17 20:29:29 +0200

    ejercicio 3 de bucle for

--- programacion1dam/src/ejerciciosfor/ejerciciopractica3.java
+++ programacion1dam/src/ejerciciosfor/ejerciciopractica3.java
@@ -1,10 +1,22 @@
 package ejerciciosfor;
 
+import java.util.Scanner;
+
 public class ejerciciopractica3 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		int n;
+		
+		Scanner sc1 = new Scanner(System.in);
+		System.out.println("Introduce una tabla de multiplicar:");
+		n = sc1.nextInt();
+		
+		for(int i =1; i <= 10; i++){
+			System.out.println(n*i);
+		}
+		sc1.close();
 	}
 
 }
--- programacion1dam/src/ejercicioswhile/ejercicio1.java
+++ programacion1dam/src/ejercicioswhile/ejercicio1.java
@@ -17,4 +17,4 @@ public class ejercicio1 {
 		
 	}
 
-}u
+}
