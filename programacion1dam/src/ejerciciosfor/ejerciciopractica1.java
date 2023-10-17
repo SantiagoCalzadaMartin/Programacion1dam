b867a50c50947a4f21bfb4f87f0d31be19522d70
El Santi <santiago.calzada.martin@colegiolitterator.com>
2023-10-17 17:22:24 +0200

    primer ejercicio de las prácticas de for

---
+++ programacion1dam/src/ejerciciosfor/ejerciciopractica1.java
@@ -0,0 +1,21 @@
+package ejerciciosfor;
+
+import java.util.Scanner;
+
+public class ejerciciopractica1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner sc1 = new Scanner(System.in);
+		System.out.println("Ingrese un número:");
+		int N = sc1.nextInt();
+		
+		for(int i = 1; i<=N; i++) {
+			
+			System.out.println("i vale " + i + " ");
+		}
+		
+	}
+
+}
