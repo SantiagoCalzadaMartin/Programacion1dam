d20e3cb4b13abb435fe195468221f0575e908c10
El Santi <santiago.calzada.martin@colegiolitterator.com>
2023-10-18 19:53:05 +0200

    ejercicio 4 resuelto

--- programacion1dam/src/ejerciciosfor/ejerciciopractica4.java
+++ programacion1dam/src/ejerciciosfor/ejerciciopractica4.java
@@ -1,15 +1,27 @@
 package ejerciciosfor;
 
+import java.util.Scanner;
+
 public class ejerciciopractica4 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 			
 	int n;
+	int factorial = 1;
+	
 	Scanner sc1 = new Scanner(System.in);
 	System.out.println("Introduce un número entero positivo para factorizarlo:");
-	n = sc1.NextInt();
-		
+	n = sc1.nextInt();
+		if(n>0) {
+			for(int i=2; i<=n; i++) {
+				factorial = i * factorial;
+				System.out.println(factorial);
+			}
+		}
+		else {
+			System.out.println("Que dices payaso");
+		}
 	}
 
 }
