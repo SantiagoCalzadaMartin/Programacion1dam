01d9ae2eeb9691d8a208072fc4a73b331d423176
El Santi <santiago.calzada.martin@colegiolitterator.com>
2023-10-18 20:02:59 +0200

    Resuelto el ejercicio 5

--- programacion1dam/src/ejerciciosfor/ejerciciopractica5.java
+++ programacion1dam/src/ejerciciosfor/ejerciciopractica5.java
@@ -1,10 +1,21 @@
 package ejerciciosfor;
 
+import java.util.Scanner;
+
 public class ejerciciopractica5 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		Scanner sc1 = new Scanner(System.in);
+		System.out.println("Ingrese un número:");
+		int N = sc1.nextInt();
+		
+		for(int i = N; i>=1; i--) {
+			
+			System.out.println("i vale " + i + " ");
+		}
+		
 	}
 
 }
