793871f22d113877be4091c622c154fe3fad9467
El Santi <santiago.calzada.martin@colegiolitterator.com>
2023-10-18 20:15:06 +0200

    Resuelto ejercicio 2 de bucles while

--- programacion1dam/src/ejercicioswhile/ejerciciopractica2.java
+++ programacion1dam/src/ejercicioswhile/ejerciciopractica2.java
@@ -5,6 +5,16 @@ public class ejerciciopractica2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		int i = 10;
+		int n = 1;
+		
+		while(i>=n) {
+			
+			System.out.println(i);
+			
+			i--;
+		}
+		
 	}
 
 }
