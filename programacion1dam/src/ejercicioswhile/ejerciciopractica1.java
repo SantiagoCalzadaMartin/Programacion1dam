9228ad3b9e6f78d6fe3f8c6fa9e65149422e8c16
El Santi <santiago.calzada.martin@colegiolitterator.com>
2023-10-18 20:09:49 +0200

    Resuelto ejercicio 1 de bucles while

--- programacion1dam/src/ejercicioswhile/ejerciciopractica1.java
+++ programacion1dam/src/ejercicioswhile/ejerciciopractica1.java
@@ -1,10 +1,24 @@
 package ejercicioswhile;
 
+import java.util.Scanner;
+
 public class ejerciciopractica1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		Scanner sc1 = new Scanner(System.in);
+		System.out.println("Ingrese un número:");
+		int n = sc1.nextInt();
+		int i = 1;
+		
+		while(i<=n) {
+			
+			System.out.println(i);
+			
+			i++;
+		}
+		
 	}
 
 }
