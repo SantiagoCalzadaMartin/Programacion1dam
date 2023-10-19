7f18d495505f05120c4cb9ada6b434a8d20c36a7
El Santi <santiago.calzada.martin@colegiolitterator.com>
2023-10-19 17:28:34 +0200

    resueto el ejercicio 3 de bucles while

---
+++ programacion1dam/src/ejercicioswhile/ejerciciopractica3.java
@@ -0,0 +1,24 @@
+package ejercicioswhile;
+
+import java.util.Scanner;
+
+public class ejerciciopractica3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner sc1 = new Scanner(System.in);
+		System.out.println("Ingrese un número:");
+		int n = sc1.nextInt();
+		int res = 0;
+
+	
+	while(n>0) {
+		
+		n=n/10;
+		res++;
+		
+	}
+	System.out.println("El número tiene " + res + " dígitos");
+	}
+}
