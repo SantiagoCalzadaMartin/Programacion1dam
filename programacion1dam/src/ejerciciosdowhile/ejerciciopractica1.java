40ae844461bcf12fbc1fec1487c8ae874dfe0211
El Santi <santiago.calzada.martin@colegiolitterator.com>
2023-10-19 18:54:38 +0200

    resuelto ejercicio 1 de bucles do while

--- programacion1dam/src/ejerciciosdowhile/ejerciciopractica1.java
+++ programacion1dam/src/ejerciciosdowhile/ejerciciopractica1.java
@@ -1,10 +1,25 @@
 package ejerciciosdowhile;
 
+import java.util.Scanner;
+
 public class ejerciciopractica1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		int n;
+		int i = 1;
+		
+		Scanner sc1 = new Scanner(System.in);
+		System.out.println("Introduce una tabla de multiplicar:");
+		n = sc1.nextInt();
+		
+		do{
+			System.out.println(n*i);
+			i++;
+		}
+		while(i <= 10) ;
+		
 	}
 
 }
