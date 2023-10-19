ba79c6316604076ef3c6eb52c33c3eca2c786e0e
El Santi <santiago.calzada.martin@colegiolitterator.com>
2023-10-19 17:50:21 +0200

    ejercicio 4 de bucles while resuelto

---
+++ programacion1dam/src/ejercicioswhile/ejerciciopracticas4.java
@@ -0,0 +1,42 @@
+package ejercicioswhile;
+
+import java.util.Scanner;
+import java.util.concurrent.ThreadLocalRandom;
+
+public class ejerciciopracticas4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int random = ThreadLocalRandom.current().nextInt(0, 100 + 1);
+		Scanner sc1 = new Scanner(System.in);
+		System.out.println("Introduzca un número del 1 al 100 para adivinarlo");
+		int n = sc1.nextInt();
+		
+		while(n!=random) {
+			
+			System.out.println("Sígue intentandolo");
+			n = sc1.nextInt();
+			
+				if(n<random) {
+					
+					System.out.println("El número es mayor a " + n );
+					
+				}
+				
+				else if(n>random) {
+					
+					System.out.println("El número es menor a " + n);
+					
+				}
+				
+				else if (n==random) {
+			
+					System.out.println("Felicidades, acertaste");
+					
+				}
+		
+		
+		}
+	}
+}
