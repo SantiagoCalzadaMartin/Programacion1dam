2ab0f4baf8f5be9660847b70db3296fd6d1ede76
El Santi <santiago.calzada.martin@colegiolitterator.com>
2023-10-20 15:54:47 +0200

    resuelto ejercicio 2 de switch

---
+++ programacion1dam/src/ejerciciosswitch/ejerciciopractica2.java
@@ -0,0 +1,52 @@
+package ejerciciosswitch;
+
+import java.util.Scanner;
+
+public class ejerciciopractica2 {
+
+	public static void main(String[] args) {
+
+		int res;
+		Scanner sc1 = new Scanner(System.in);
+		System.out.println("Ingresae un número");
+		int n = sc1.nextInt();
+		Scanner sc2 = new Scanner(System.in);
+		System.out.println("Introduzca un operador de + - * / ");
+		char N = sc2.next().charAt(0);
+		Scanner sc3 = new Scanner(System.in);
+		System.out.println("Introduzca el segundo número");
+		int n2 = sc3.nextInt();
+		
+		switch(N) {
+		case '+':
+			res = n + n2;
+			System.out.println("El resultado es: " + res);
+			break;
+		case '-':
+			res = n - n2;
+			System.out.println("El resultado es: " + res);
+			break;
+		case '*':
+			res = n * n2;
+			System.out.println("El resultado es: " + res);
+			break;
+		case '/':
+			if(n2 != 0) {
+				res = n / n2;
+				System.out.println("El resultado es: " + res);
+			}
+			else {
+				System.out.println("Error: División por cero no permitida");
+			}
+			break;
+			default:
+				System.out.println("Operador no válido");
+		
+		}
+		sc1.close();
+		sc2.close();
+		sc3.close();
+		
+	}
+
+}
