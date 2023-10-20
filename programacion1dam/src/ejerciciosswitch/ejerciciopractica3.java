966e3dd6ea4123d03d72cce9e764a07847d794ee
El Santi <santiago.calzada.martin@colegiolitterator.com>
2023-10-20 17:45:44 +0200

    Cambios hechos por el Sierra para estar más bonito

--- programacion1dam/src/ejerciciosswitch/ejerciciopractica3.java
+++ programacion1dam/src/ejerciciosswitch/ejerciciopractica3.java
@@ -12,21 +12,31 @@ public class ejerciciopractica3 {
 		Scanner sc2 = new Scanner(System.in);
 		System.out.println("Ingrese su moneda en euros (E) o dólares (D)");
 		char N = sc2.next().charAt(0);
-		Scanner sc3 = new Scanner(System.in);
-		System.out.println("¿Quieres cambiar de moneda? si (Y) no (N)");
-		char n2 = sc3.next().charAt(0);
 		
-		if(n2 == 'Y') {
+		
 			switch(N) {
 			case 'E':
-				n = n * 1.06;
-				System.out.println("Tienes " + n + " Dólares");
+				System.out.println("Tienes " + n + " €");
 				break;
 			default:
-				n = n / 1.06;
-				System.out.println("Tienes " + n + " Euros");
+				System.out.println("Tienes " + n + " $");
 			}
-		}
+			
+			Scanner sc3 = new Scanner(System.in);
+			System.out.println("¿Quieres cambiar de moneda? si (Y) no (N)");
+			char n2 = sc3.next().charAt(0);
+			if(n2 == 'Y') {
+				switch(N) {
+				case 'E':
+					n = n * 1.06;
+					System.out.println("Tienes " + n + " $");
+					break;
+				case 'D':
+					n = n / 1.06;
+					System.out.println("Tienes " + n + " €");
+				}
+				
+			}
 		else {
 			System.out.println("Tienes " + n + N);
 		}
