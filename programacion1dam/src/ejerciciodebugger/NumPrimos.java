e79a083460e4d67bd8010f32568ebf23f618a577
El Santi <santiago.calzada.martin@colegiolitterator.com>
2023-10-24 17:14:46 +0200

    Ejercicio resuelto de numeros primos

---
+++ programacion1dam/src/ejerciciodebugger/NumPrimos.java
@@ -0,0 +1,32 @@
+package ejerciciodebugger;
+import java.util.Scanner;
+
+//usando un bucle while quiero que recorra los 10.000 primeros numeros y caudno detectemos
+// un primo numero, se imprima por pantalla, ademas imprimir que posicion nº primo es
+
+
+public class NumPrimos {
+
+	public static void main(String[] args) {
+		int i = 1;
+        int cont = 1;
+
+        while (i <= 10000) {
+            int Primo = 1;
+
+            for (int h = 2; h * h <= i; h++) {
+                if (i % h == 0) {
+                    Primo = 0;
+                    break;
+                }
+            }
+
+            if (Primo == 1) {
+                System.out.println("Numero primo " + i + " y ocupa la posición " + cont);
+                cont++;
+            }
+
+            i++;
+        }
+    }
+}
