3201e41aa91234b7db3ff46e9b09279d42324ec3
El Santi <santiago.calzada.martin@colegiolitterator.com>
2023-10-24 19:01:43 +0200

    resuelto ejercicio 2 de bucles anidados

--- programacion1dam/src/ejercicioforanidado/ejercicopractica2.java
+++ programacion1dam/src/ejercicioforanidado/ejercicopractica2.java
@@ -6,24 +6,26 @@ import java.util.Scanner;
  * @author santi
  * Esta es la unica clase de mi programa
  */
-public class ejerciciopractica1 {
-	/**
-	 * Este metodo es el punto de entrada de mi programa
-	 * @param args
-	 */
-	
+public class ejercicopractica2 {
+	 /**
+	  * Este metodo es el punto de entrada de mi programa
+	  * @param args
+	  */
+
 	public static void main(String[] args) {
-		
 		Scanner sc1 = new Scanner(System.in);
 		System.out.println("Ingrese un número para hacer un triángulo:");
 		int n = sc1.nextInt();
 		
+		
 		for ( int i = 0; i <= n; i++ ) {
+			int k = 1;
 			for ( int j = 1; j <= i; j++ ) {
-				System.out.print("*");
+				
+				System.out.print(k);
+				k++;
 			}
 			System.out.println("");
 		}
 	}
-
 }
