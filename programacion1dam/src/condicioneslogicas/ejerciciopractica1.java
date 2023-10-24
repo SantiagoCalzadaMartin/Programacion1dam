0fa78675476e4020f7c16267a983386bc1657119
El Santi <santiago.calzada.martin@colegiolitterator.com>
2023-10-24 20:34:11 +0200

    resuelto el ejercicio 1 de condiciones lógicas

---
+++ programacion1dam/src/condicioneslogicas/ejerciciopractica1.java
@@ -0,0 +1,28 @@
+package condicioneslogicas;
+
+import java.util.Scanner;
+
+public class ejerciciopractica1 {
+
+	public static void main(String[] args) {
+
+		Scanner sc1 = new Scanner(System.in);
+		System.out.println("¿Cuantos ángulos iguales tiene tu triángulo?");
+		int n = sc1.nextInt();
+		
+		if (n == 0 || n == 1) {
+			System.out.println("Tu triángulo es escaleno");
+		}
+		else if (n == 2) {
+			System.out.println("Tu triángulo es isósceles");
+		}
+		else if (n == 3) {
+			System.out.println("Tu triángulo es equilátero");
+		}
+		else {
+			System.out.println("Los triángulos tienen tres lados");
+		}
+
+	}
+
+}
