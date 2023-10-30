29d95aa8e21fd9caa11167c98d29df19d77ed794
El Santi <santiago.calzada.martin@colegiolitterator.com>
2023-10-30 19:11:25 +0100

    Ejercicio 5 de stringchar resuelto

---
+++ programacion1dam/src/StringChar/ejerciciopractica5.java
@@ -0,0 +1,37 @@
+package StringChar;
+
+import java.util.Scanner;
+
+/**
+ * @author santi
+ * Esta es la unica clase de mi programa
+ */
+
+public class ejerciciopractica5 {
+
+	/**
+	 * Este metodo es el punto de entrada de mi programa
+	 * @param args
+	 */
+	public static String invertir (String cadena) {
+
+		return new StringBuilder(cadena).reverse().toString();
+
+	}
+	
+	public static void main(String[] args) {
+		
+		Scanner sc1 = new Scanner(System.in);
+		System.out.println("Ingrese un frase y te decimos si es un palíndromo");
+		String n = sc1.nextLine();
+		String invert = invertir(n);
+		
+		if (n.equals(invert)) 
+			System.out.println("Es un palíndromo");
+		
+		else 
+			System.out.println("No es un palíndromo");
+		
+	}
+
+}
