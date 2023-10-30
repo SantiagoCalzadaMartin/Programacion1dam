d513140becf2ab6d32742875a09ca462dab82194
El Santi <santiago.calzada.martin@colegiolitterator.com>
2023-10-30 20:25:02 +0100

    ejercicio 1 de enum resuelto

---
+++ programacion1dam/src/Enum/ejerciciopractica1.java
@@ -0,0 +1,38 @@
+package Enum;
+
+import java.util.Scanner;
+
+/**
+ * @author santi
+ * Esta es la unica clase de mi programa
+ */
+
+public class ejerciciopractica1 {
+
+	/**
+	 * Este metodo es el punto de entrada de mi programa
+	 * @param args
+	 */
+	
+	enum DiaSemana {
+        LUNES, MARTES, MIERCOLES, JUEVES, VIERNES, SABADO, DOMINGO
+    }
+    public static void main(String[] args) {
+
+         Scanner sc1 = new Scanner(System.in);
+
+            System.out.println("Ingrese un número entre 1 y 7:");
+            int n = sc1.nextInt();
+
+            if(n>=8 || n<=0) {
+                System.out.println("a donde va tu flipao");
+            }else {
+
+
+            DiaSemana dia = DiaSemana.values()[n - 1];
+            System.out.println("El dia de la semana correspondiente al número " + n + " es: " + dia);
+
+
+            }
+    }
+}
